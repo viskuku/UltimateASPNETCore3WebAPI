2cfa9abf9bdfb6fe63190eb0bd1e68aa6c1a4a19
Claudinei Felix <claudinei.felix@outlook.com>
2020-11-26 13:17:01 -0300

    17-Filtering

--- UltimateASPNETCore3WebAPI/Controllers/EmployeesController.cs
+++ UltimateASPNETCore3WebAPI/Controllers/EmployeesController.cs
@@ -34,6 +34,9 @@ namespace UltimateASPNETCore3WebAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> GetEmployeesForCompany(Guid companyId, [FromQuery] EmployeeParameters employeeParameters)
         {
+            if(!employeeParameters.ValidAgeRange) 
+                return BadRequest("Max age can't be less than min age.");
+            
             var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges:
            false);
             if (company == null)
